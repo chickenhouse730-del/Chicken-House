0e627d387d102815f4f88135f6031b84167a4d9b
chickenhouse730-del <chickenhouse730@gmail.com>
2026-08-14 23:13:36 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE = "chicken-house-v1";
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(c => c.addAll(["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"])));
+});
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(r => r || fetch(event.request)));
+});
